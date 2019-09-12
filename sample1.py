46665b922699a049771ad39aa4e78ca1fb7243b4
Dedi Mulyadi <dedimu@gmail.com>
2019-09-12 19:39:03 +0700

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -1,5 +1,9 @@
+klik button windows
+cmd
 cd \python27
 python
+
+output:
 Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license" for more information.
 >>> a=5
