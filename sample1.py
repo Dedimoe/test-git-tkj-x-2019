da2b01a161e72292dc36e30778c21c393490373a
Dedi Mulyadi <dedimu@gmail.com>
2019-09-12 18:29:54 +0700

    Create sample1.py

---
+++ sample1.py
@@ -0,0 +1,35 @@
+cd \python27
+python
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> a=5
+>>> b=7
+>>> c=a+b
+>>> print (c)
+12
+>>> print (b)
+7
+>>> b=12
+>>> print (b)
+12
+>>> print (z)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'z' is not defined
+>>> z=1000000
+>>> print (z)
+1000000
+>>> z=z*2
+>>> print (z)
+2000000
+>>> if z==2000000:
+...  print ("uang saya")
+...
+uang saya
+>>> if z==2000000:
+...  print ("uang saya dong")
+...
+uang saya dong
+>>>
+>>> ^Z
+
